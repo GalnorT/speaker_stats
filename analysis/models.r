69292a2e431468e94510b6e4be26e645321de8fb
Tomas Galnor <tomas.galnor@cz.ey.com>
2026-01-29 21:52:31 +0100

    refactoring

--- analysis/models.r
+++ analysis/models.r
@@ -5,6 +5,56 @@ library(plm)
 library(stringr)
 library(lmtest)
 
+run_and_print <- function(label, model) {
+    cat("\n===", label, "===\n")
+    print(summary(model))
+}
+
+build_model_data <- function(data, avg_first10_table = NULL) {
+    data_use <- data
+    if (!is.null(avg_first10_table)) {
+        data_use <- data_use %>%
+            select(-avg_first10_score) %>%
+            inner_join(avg_first10_table, by = c("speaker_name_clean" = "name_clean"))
+    }
+
+    data_use %>%
+        filter(
+            !is.na(speaker_points),
+            !is.na(avg_first10_score),
+            !is.na(lag_avg_teammate_score),
+            !is.na(tournament_round),
+            !is.na(motion_balance),
+            !is.na(motion_balance_x_aff)
+        ) %>%
+        mutate(speaker_id = as.integer(as.factor(speaker_name_clean))) %>%
+        arrange(speaker_id, debate_date)
+}
+
+make_mundlak_data <- function(model_df, min_obs = 10) {
+    model_df %>%
+        arrange(speaker_id, debate_date, debate_id) %>%
+        group_by(speaker_id) %>%
+        mutate(time_index = row_number()) %>%
+        ungroup() %>%
+        group_by(speaker_id) %>%
+        filter(n() >= min_obs) %>%
+        mutate(
+            mean_lag_avg_teammate_score = mean(lag_avg_teammate_score, na.rm = TRUE),
+            mean_tournament_round = mean(tournament_round, na.rm = TRUE),
+            lag_avg_teammate_score_c = lag_avg_teammate_score - mean_lag_avg_teammate_score,
+            tournament_round_c = tournament_round - mean_tournament_round
+        ) %>%
+        ungroup() %>%
+        mutate(
+            avg_first10_score_z = as.numeric(scale(avg_first10_score)),
+            lag_avg_teammate_score_c_z = as.numeric(scale(lag_avg_teammate_score_c)),
+            tournament_round_c_z = as.numeric(scale(tournament_round_c)),
+            mean_lag_avg_teammate_score_z = as.numeric(scale(mean_lag_avg_teammate_score)),
+            mean_tournament_round_z = as.numeric(scale(mean_tournament_round))
+        )
+}
+
 base_dir <- "c:/Users/QX163MQ/Downloads/non-work/school/data processing in python/project/speaker_stats"
 scores_path <- file.path(base_dir, "scraping/debate_natural_ability_scraping/speaker_scores.csv")
 final_path  <- file.path(base_dir, "data/processed/final_joined_data.csv")
@@ -76,120 +126,45 @@ df <- df %>%
     ungroup()
 
 # Model data
-df_model <- df %>%
-    filter(
-        !is.na(speaker_points),
-        !is.na(avg_first10_score),
-        !is.na(lag_avg_teammate_score),
-        !is.na(tournament_round),
-        !is.na(motion_balance),
-        !is.na(motion_balance_x_aff)
-    ) %>%
-    mutate(speaker_id = as.integer(as.factor(speaker_name_clean))) %>%
-    arrange(speaker_id, debate_date)
-
-df_model_strict <- df %>%
-    select(-avg_first10_score) %>%
-    inner_join(avg_first10_strict, by = c("speaker_name_clean" = "name_clean")) %>%
-    filter(
-        !is.na(speaker_points),
-        !is.na(avg_first10_score),
-        !is.na(lag_avg_teammate_score),
-        !is.na(tournament_round),
-        !is.na(motion_balance),
-        !is.na(motion_balance_x_aff)
-    ) %>%
-    mutate(speaker_id = as.integer(as.factor(speaker_name_clean))) %>%
-    arrange(speaker_id, debate_date)
+df_model <- build_model_data(df)
+df_model_strict <- build_model_data(df, avg_first10_table = avg_first10_strict)
 
 # Pooled OLS
 pool_formula <- speaker_points ~ avg_first10_score + is_male + lag_avg_teammate_score +
     tournament_round
 
-pool_mod <- plm(pool_formula, data = df_model, model = "pooling", index = c("speaker_id", "debate_date"))
+run_panel_models <- function(data, formula, index = c("speaker_id", "debate_date")) {
+    pool <- plm(formula, data = data, model = "pooling", index = index)
+    re <- plm(formula, data = data, model = "random", index = index)
+    fe <- plm(formula, data = data, model = "within", index = index)
 
-# Random Effects
-re_mod <- plm(pool_formula, data = df_model, model = "random", index = c("speaker_id", "debate_date"))
-
-# Fixed Effects (proxy will be dropped if time-invariant)
-fe_mod <- plm(pool_formula, data = df_model, model = "within", index = c("speaker_id", "debate_date"))
-
-# LM test: pooled vs RE
-lm_test <- plmtest(pool_mod, type = "bp")
-
-# Hausman test: RE vs FE
-hausman_test <- phtest(fe_mod, re_mod)
-
-pool_mod_strict <- plm(pool_formula, data = df_model_strict, model = "pooling", index = c("speaker_id", "debate_date"))
-
-re_mod_strict <- plm(pool_formula, data = df_model_strict, model = "random", index = c("speaker_id", "debate_date"))
-
-fe_mod_strict <- plm(pool_formula, data = df_model_strict, model = "within", index = c("speaker_id", "debate_date"))
-
-lm_test_strict <- plmtest(pool_mod_strict, type = "bp")
-
-hausman_test_strict <- phtest(fe_mod_strict, re_mod_strict)
-
-print(summary(pool_mod))
-print(summary(re_mod))
-print(summary(fe_mod))
-print(lm_test)
-print(hausman_test)
-
-print(summary(pool_mod_strict))
-print(summary(re_mod_strict))
-print(summary(fe_mod_strict))
-print(lm_test_strict)
-print(hausman_test_strict)
-
-
-df_model_proper_time <- df_model %>%
-    arrange(speaker_id, debate_date, debate_id) %>%
-    group_by(speaker_id) %>%
-    mutate(time_index = row_number()) %>%
-    ungroup()
-
-df_model_proper_time_strict <- df_model_strict %>%
-    arrange(speaker_id, debate_date, debate_id) %>%
-    group_by(speaker_id) %>%
-    mutate(time_index = row_number()) %>%
-    ungroup()
-
-df_mundlak <- df_model_proper_time %>%
-    group_by(speaker_id) %>%
-    filter(n() >= 10) %>%
-    mutate(
-        mean_lag_avg_teammate_score = mean(lag_avg_teammate_score, na.rm = TRUE),
-        mean_tournament_round = mean(tournament_round, na.rm = TRUE),
-        lag_avg_teammate_score_c = lag_avg_teammate_score - mean_lag_avg_teammate_score,
-        tournament_round_c = tournament_round - mean_tournament_round
-    ) %>%
-    ungroup() %>%
-    mutate(
-        avg_first10_score_z = as.numeric(scale(avg_first10_score)),
-        lag_avg_teammate_score_c_z = as.numeric(scale(lag_avg_teammate_score_c)),
-        tournament_round_c_z = as.numeric(scale(tournament_round_c)),
-        mean_lag_avg_teammate_score_z = as.numeric(scale(mean_lag_avg_teammate_score)),
-        mean_tournament_round_z = as.numeric(scale(mean_tournament_round))
+    list(
+        pool = pool,
+        re = re,
+        fe = fe,
+        lm_test = plmtest(pool, type = "bp"),
+        hausman_test = phtest(fe, re)
     )
+}
 
-df_mundlak_strict <- df_model_proper_time_strict %>%
-    group_by(speaker_id) %>%
-    filter(n() >= 10) %>%
-    mutate(
-        mean_lag_avg_teammate_score = mean(lag_avg_teammate_score, na.rm = TRUE),
-        mean_tournament_round = mean(tournament_round, na.rm = TRUE),
-        lag_avg_teammate_score_c = lag_avg_teammate_score - mean_lag_avg_teammate_score,
-        tournament_round_c = tournament_round - mean_tournament_round
-    ) %>%
-    ungroup() %>%
-    mutate(
-        avg_first10_score_z = as.numeric(scale(avg_first10_score)),
-        lag_avg_teammate_score_c_z = as.numeric(scale(lag_avg_teammate_score_c)),
-        tournament_round_c_z = as.numeric(scale(tournament_round_c)),
-        mean_lag_avg_teammate_score_z = as.numeric(scale(mean_lag_avg_teammate_score)),
-        mean_tournament_round_z = as.numeric(scale(mean_tournament_round))
-    )
+panel_models <- run_panel_models(df_model, pool_formula)
+panel_models_strict <- run_panel_models(df_model_strict, pool_formula)
+
+run_and_print("Pooled OLS", panel_models$pool)
+run_and_print("Random Effects", panel_models$re)
+run_and_print("Fixed Effects", panel_models$fe)
+print(panel_models$lm_test)
+print(panel_models$hausman_test)
+
+run_and_print("Pooled OLS (Strict)", panel_models_strict$pool)
+run_and_print("Random Effects (Strict)", panel_models_strict$re)
+run_and_print("Fixed Effects (Strict)", panel_models_strict$fe)
+print(panel_models_strict$lm_test)
+print(panel_models_strict$hausman_test)
+
+
+df_mundlak <- make_mundlak_data(df_model, min_obs = 10)
+df_mundlak_strict <- make_mundlak_data(df_model_strict, min_obs = 10)
 
 colinearity_test <- lm(speaker_points ~ lag_avg_teammate_score_c + mean_lag_avg_teammate_score + tournament_round_c + mean_tournament_round, data = df_mundlak)
 summary(colinearity_test)
@@ -198,22 +173,53 @@ mundlak_formula <- speaker_points ~ avg_first10_score_z + is_male +
     lag_avg_teammate_score_c_z + tournament_round_c_z +
     mean_lag_avg_teammate_score_z + mean_tournament_round_z
 
-mundlak_mod <- plm(
-    mundlak_formula,
-    data = df_mundlak,
-    model = "random",
-    index = c("speaker_id", "time_index")
+mundlak_formulas <- list(
+    full = mundlak_formula,
+    no_mean_round = speaker_points ~ avg_first10_score_z + is_male +
+        lag_avg_teammate_score_c_z + tournament_round_c_z +
+        mean_lag_avg_teammate_score_z,
+    no_mean_lag = speaker_points ~ avg_first10_score_z + is_male +
+        lag_avg_teammate_score_c_z + tournament_round_c_z +
+        mean_tournament_round_z
 )
 
-mundlak_mod_strict <- plm(
-    mundlak_formula,
-    data = df_mundlak_strict,
-    model = "random",
-    index = c("speaker_id", "time_index")
-)
+run_mundlak_models <- function(formula, data, label_prefix = "") {
+    mod <- plm(
+        formula,
+        data = data,
+        model = "random",
+        index = c("speaker_id", "time_index")
+    )
+    run_and_print(paste0(label_prefix, "Mundlak"), mod)
+    mod
+}
 
-print(summary(mundlak_mod))
-print(summary(mundlak_mod_strict))
+run_mundlak_pooled <- function(formula, data, label_prefix = "") {
+    mod <- plm(
+        formula,
+        data = data,
+        model = "pooling",
+        index = c("speaker_id", "time_index")
+    )
+    cat("\n===", label_prefix, "Mundlak pooled (clustered SEs)", "===\n")
+    print(coeftest(mod, vcov = vcovHC(mod, type = "HC1", cluster = "group")))
+    mod
+}
+
+mundlak_mod <- run_mundlak_models(mundlak_formulas$full, df_mundlak)
+mundlak_mod_strict <- run_mundlak_models(mundlak_formulas$full, df_mundlak_strict, "Strict ")
+
+
+# mundlak with dropped mean terms
+mundlak_no_mean_round <- run_mundlak_models(mundlak_formulas$no_mean_round, df_mundlak, "No mean round ")
+mundlak_no_mean_round_strict <- run_mundlak_models(mundlak_formulas$no_mean_round, df_mundlak_strict, "No mean round (Strict) ")
+
+mundlak_no_mean_lag <- run_mundlak_models(mundlak_formulas$no_mean_lag, df_mundlak, "No mean lag ")
+mundlak_no_mean_lag_strict <- run_mundlak_models(mundlak_formulas$no_mean_lag, df_mundlak_strict, "No mean lag (Strict) ")
+
+mundlak_pooled <- run_mundlak_pooled(mundlak_formulas$full, df_mundlak)
+mundlak_pooled_strict <- run_mundlak_pooled(mundlak_formulas$full, df_mundlak_strict, "Strict ")
+
 
 # DEBUG
 df_mundlak %>%
@@ -238,69 +244,6 @@ df_mundlak %>%
     filter(const_lag | const_round)
 
 
-# mundlak with dropped mean terms
-
-## mundlak with dropped mean tourney round
-mundlak_formula_no_mean_round <- speaker_points ~ avg_first10_score_z + is_male +
-    lag_avg_teammate_score_c_z + tournament_round_c_z +
-    mean_lag_avg_teammate_score_z
-
-mundlak_no_mean_round <- plm(
-    mundlak_formula_no_mean_round,
-    data = df_mundlak,
-    model = "random",
-    index = c("speaker_id", "time_index")
-)
-
-mundlak_no_mean_round_strict <- plm(
-    mundlak_formula_no_mean_round,
-    data = df_mundlak_strict,
-    model = "random",
-    index = c("speaker_id", "time_index")
-)
-
-print(summary(mundlak_no_mean_round))
-print(summary(mundlak_no_mean_round_strict))
-
-## mundlak with dropped mean lag avg score 
-mundlak_formula_no_mean_lag <- speaker_points ~ avg_first10_score_z + is_male +
-    lag_avg_teammate_score_c_z + tournament_round_c_z +
-    mean_tournament_round_z
-
-mundlak_no_mean_lag <- plm(
-    mundlak_formula_no_mean_lag,
-    data = df_mundlak,
-    model = "random",
-    index = c("speaker_id", "time_index")
-)
-
-mundlak_no_mean_lag_strict <- plm(
-    mundlak_formula_no_mean_lag,
-    data = df_mundlak_strict,
-    model = "random",
-    index = c("speaker_id", "time_index")
-)
-
-print(summary(mundlak_no_mean_lag))
-print(summary(mundlak_no_mean_lag_strict))
-
-mundlak_pooled <- plm(
-    mundlak_formula,
-    data = df_mundlak,
-    model = "pooling",
-    index = c("speaker_id", "time_index")
-)
-
-mundlak_pooled_strict <- plm(
-    mundlak_formula,
-    data = df_mundlak_strict,
-    model = "pooling",
-    index = c("speaker_id", "time_index")
-)
-
-print(coeftest(mundlak_pooled, vcov = vcovHC(mundlak_pooled, type = "HC1", cluster = "group")))
-print(coeftest(mundlak_pooled_strict, vcov = vcovHC(mundlak_pooled_strict, type = "HC1", cluster = "group")))
-
 gmm_formula <- speaker_points ~ lag(speaker_points, 1) + avg_first10_score_z + is_male +
     lag_avg_teammate_score_c_z + tournament_round_c_z +
     mean_lag_avg_teammate_score_z + mean_tournament_round_z |
@@ -324,12 +267,12 @@ gmm_mod_strict <- pgmm(
     index = c("speaker_id", "time_index")
 )
 
-print(summary(gmm_mod))
-print(summary(gmm_mod_strict))
+run_and_print("GMM", gmm_mod)
+run_and_print("GMM Strict", gmm_mod_strict)
 
 
 
-#Check rank deficiency:
+#Check rank deficienc:
 m <- model.matrix(mundlak_formula, df_mundlak)
 qr(m)$rank; ncol(m)
 
@@ -341,7 +284,7 @@ alias(lm(mundlak_formula, data = df_mundlak))
 #Check duplicate (speaker_id, time_index) rows:
 df_mundlak %>% count(speaker_id, time_index) %>% filter(n > 1)
 
-# duplicate rows?s
+# duplicate rows?
 
 ercomp(mundlak_formula, data = df_mundlak, model = "random", effect = "individual", random.method = "swar", index = c("speaker_id", "time_index"))
 
