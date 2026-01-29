316548860684732ac1e9ab411351575c63338859
Tomas Galnor <tomas.galnor@cz.ey.com>
2026-01-29 21:10:30 +0100

    switched to using lagged values of teammate avg score to avoid simultaneity

--- analysis/models.r
+++ analysis/models.r
@@ -68,6 +68,10 @@ get_teammate_avg <- function(x) {
 df <- df %>%
     group_by(debate_id, side) %>%
     mutate(avg_teammate_score = get_teammate_avg(speaker_points)) %>%
+    ungroup() %>%
+    arrange(speaker_name_clean, debate_date, debate_id) %>%
+    group_by(speaker_name_clean) %>%
+    mutate(lag_avg_teammate_score = lag(cummean(avg_teammate_score))) %>%
     ungroup()
 
 # Model data
@@ -75,7 +79,7 @@ df_model <- df %>%
     filter(
         !is.na(speaker_points),
         !is.na(avg_first10_score),
-        !is.na(avg_teammate_score),
+        !is.na(lag_avg_teammate_score),
         !is.na(tournament_round),
         !is.na(motion_balance),
         !is.na(motion_balance_x_aff)
@@ -89,7 +93,7 @@ df_model_strict <- df %>%
     filter(
         !is.na(speaker_points),
         !is.na(avg_first10_score),
-        !is.na(avg_teammate_score),
+        !is.na(lag_avg_teammate_score),
         !is.na(tournament_round),
         !is.na(motion_balance),
         !is.na(motion_balance_x_aff)
@@ -98,7 +102,7 @@ df_model_strict <- df %>%
     arrange(speaker_id, debate_date)
 
 # Pooled OLS
-pool_formula <- speaker_points ~ avg_first10_score + is_male + avg_teammate_score +
+pool_formula <- speaker_points ~ avg_first10_score + is_male + lag_avg_teammate_score +
     tournament_round
 
 pool_mod <- plm(pool_formula, data = df_model, model = "pooling", index = c("speaker_id", "debate_date"))
@@ -144,20 +148,37 @@ df_model_proper_time <- df_model %>%
     mutate(time_index = row_number()) %>%
     ungroup()
 
+df_model_proper_time_strict <- df_model_strict %>%
+    arrange(speaker_id, debate_date, debate_id) %>%
+    group_by(speaker_id) %>%
+    mutate(time_index = row_number()) %>%
+    ungroup()
+
 df_mundlak <- df_model_proper_time %>%
     group_by(speaker_id) %>%
     filter(n() >= 2) %>%
     mutate(
-        mean_avg_teammate_score = mean(avg_teammate_score, na.rm = TRUE),
+        mean_lag_avg_teammate_score = mean(lag_avg_teammate_score, na.rm = TRUE),
         mean_tournament_round = mean(tournament_round, na.rm = TRUE),
-        avg_teammate_score_c = avg_teammate_score - mean_avg_teammate_score,
+        lag_avg_teammate_score_c = lag_avg_teammate_score - mean_lag_avg_teammate_score,
+        tournament_round_c = tournament_round - mean_tournament_round
+    ) %>%
+    ungroup()
+
+df_mundlak_strict <- df_model_proper_time_strict %>%
+    group_by(speaker_id) %>%
+    filter(n() >= 2) %>%
+    mutate(
+        mean_lag_avg_teammate_score = mean(lag_avg_teammate_score, na.rm = TRUE),
+        mean_tournament_round = mean(tournament_round, na.rm = TRUE),
+        lag_avg_teammate_score_c = lag_avg_teammate_score - mean_lag_avg_teammate_score,
         tournament_round_c = tournament_round - mean_tournament_round
     ) %>%
     ungroup()
 
 mundlak_formula <- speaker_points ~ avg_first10_score + is_male +
-    avg_teammate_score_c + tournament_round_c +
-    mean_avg_teammate_score + mean_tournament_round
+    lag_avg_teammate_score_c + tournament_round_c +
+    mean_lag_avg_teammate_score + mean_tournament_round
 
 mundlak_mod <- plm(
     mundlak_formula,
@@ -166,4 +187,12 @@ mundlak_mod <- plm(
     index = c("speaker_id", "time_index")
 )
 
+mundlak_mod_strict <- plm(
+    mundlak_formula,
+    data = df_mundlak_strict,
+    model = "random",
+    index = c("speaker_id", "time_index")
+)
+
 print(summary(mundlak_mod))
+print(summary(mundlak_mod_strict))
